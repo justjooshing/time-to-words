be872bb86cb57c1fea4854bfbbef8333160d9ebc
Josh <joshua.l.mertens@gmail.com>
2024-03-15 14:51:29 +1100

    feat: create function which returns time in words

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
+const timeInNumbers = (time) => {
+  const [strHours, strMinutes] = time.split(":");
+
+  return { hours: Number(strHours), minutes: Number(strMinutes) };
+};
+
+const convertUnitToWord = (num) =>
+  ({
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "quarter",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    21: "twenty-one",
+    22: "twenty-two",
+    23: "twenty-three",
+    24: "twenty-four",
+    25: "twenty-five",
+    26: "twenty-six",
+    27: "twenty-seven",
+    28: "twenty-eight",
+    29: "twenty-nine",
+    30: "half",
+  }[num]);
+
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
+  const { hours, minutes } = timeInNumbers(time);
+  const modifier = minutes <= 30 ? "past" : "to";
 
-  return 'half past eight';
+  const convertedMinutes = convertUnitToWord(
+    minutes >= 30 ? 60 - minutes : minutes
+  );
+  const convertedHours = convertUnitToWord(minutes > 30 ? hours + 1 : hours);
+
+  if (time === "0:00") return "midnight";
+  if (time === "12:00") return "midday";
+  if (minutes === 0) return `${convertUnitToWord(hours)} o'clock`;
+
+  return `${convertedMinutes} ${modifier} ${convertedHours}`;
 }
 
 module.exports = { convertTimeToWords };
